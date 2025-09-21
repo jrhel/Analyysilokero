f730d9a4d1a54367e908e6c8a16a6534d18e9120
jrhel <johannes.riska@helsinki.fi>
2025-09-21 15:20:23 +0300

    Created user interface for entering and updating eneterd hypothesis in users analysis.

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.secret_key = "17fa24cf6a2ad4dac44a43963dd2c43e"
 
 @app.route("/")
 def index():
-    if in_session():
+    if "username" in session.keys():
         return redirect("/user_page")
     else:
         return render_template("index.html")
@@ -56,10 +56,24 @@ def user_page():
 
 @app.route("/sign_out", methods=["POST"])
 def sign_out():
-    del session["username"]
+    session.clear()
+    # del session["username"]
     return redirect("/")
 
-def in_session():
-    if "username" in session.keys():
-        return True
-    return False
+@app.route("/new_hypothesis", methods=["POST"])
+def new_hypothesis():
+    new_hypothesis = request.form["new_hypothesis"]
+    if "known_hypotheses" in session.keys():
+        known_hypotheses = session["known_hypotheses"]
+        known_hypotheses.append(new_hypothesis)
+        session["known_hypotheses"] = known_hypotheses
+    else:
+        session["known_hypotheses"] = [new_hypothesis]
+    return redirect("/analysis", code=307)
+
+@app.route("/analysis", methods=["POST"])
+def analysis():
+    known_hypotheses = []
+    if "known_hypotheses" in session.keys():
+        known_hypotheses = session["known_hypotheses"]
+    return render_template("analysis/index.html", hypotheses=known_hypotheses)
